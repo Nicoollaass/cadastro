17f4c56fd91feef783e61f15a49bd17e9ad02edc
unknown <nicolas.ssantos@CAS419288C14735.senacsp.edu.br>
2015-08-28 21:46:49 -0300

    finalização da interface com menu de opções

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -11,43 +11,65 @@ import java.util.Scanner;
  * @author HERICK
  */
 public class Interface {
+
     public static void main(String[] args) {
         CrudPessoa pessoa = new CrudPessoa();
-        /*Scanner entrada = new Scanner(System.in);
+        Scanner entradaInt = new Scanner(System.in);
+        Scanner entradaString = new Scanner(System.in);
         
-        System.out.println("Digite seu nome:");
-        pessoa.setNome(entrada.nextLine());        
-        System.out.println("Digite sua data de Nascimento:");
-        pessoa.setDate(entrada.nextLine());
-        System.out.println("Digite E-mail:");
-        pessoa.setEmail(entrada.nextLine());
-        System.out.println("Digite Telefone:");
-        pessoa.setTelefone(entrada.nextLine());*/
-        
-        
-        //metodo que salva pessoa
-        /*pessoa.setNome("Nicolas silva dos santos");
-        pessoa.setEmail("email@email.com");
-        pessoa.setDate("05/11/1995");
-        pessoa.setTelefone("56227332");
-        System.out.println(pessoa.salvar());
-        */
-        
-        //metodo que edita pessoa
-        /*pessoa.setId(1);
-        pessoa.setNome("Nicola");
-        pessoa.setEmail("email@teste.com");
-        pessoa.setDate("05/11/1995");
-        pessoa.setTelefone("56227332");
-        System.out.println(pessoa.editar());*/
-        
-        //metodo que exclui pessoa
-        //pessoa.excluir(1);
-        
-        //metodo para buscar e listar pessoas cadastradas
-        //pessoa.listarPessoas(pessoa.buscarPessoas());
-        
-        
-        
+        int opcao;
+        do {
+            System.out.println("=====Menu=====\n"
+                    + "1 - Salvar\n"
+                    + "2 - Editar\n"
+                    + "3 - Buscar\n"
+                    + "4 - Excluir\n"
+                    + "5 - Sair\n");
+            System.out.println("Digite uma opção: ");
+            opcao = entradaInt.nextInt();
+            
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite seu nome:");
+                    pessoa.setNome(entradaString.nextLine());
+                    System.out.println("Digite sua data de Nascimento:");
+                    pessoa.setDate(entradaString.nextLine());
+                    System.out.println("Digite E-mail:");
+                    pessoa.setEmail(entradaString.nextLine());
+                    System.out.println("Digite Telefone:");
+                    pessoa.setTelefone(entradaString.nextLine());
+                    System.out.println(pessoa.salvar());
+                    break;
+                case 2:
+                    pessoa.listarPessoas(pessoa.buscarPessoas());
+                    System.out.println("Digite um ID de pessoa que você queira editar: ");
+                    pessoa.setId(entradaInt.nextInt());
+                    System.out.println("Digite um nome: ");
+                    pessoa.setNome(entradaString.nextLine());
+                    System.out.println("Digite sua data de Nascimento:");
+                    pessoa.setDate(entradaString.nextLine());
+                    System.out.println("Digite E-mail:");
+                    pessoa.setEmail(entradaString.nextLine());
+                    System.out.println("Digite Telefone:");
+                    pessoa.setTelefone(entradaString.nextLine());
+                    System.out.println(pessoa.editar());
+                    break;
+                case 3:
+                    pessoa.listarPessoas(pessoa.buscarPessoas());                    
+                    break;
+                case 4:
+                    pessoa.listarPessoas(pessoa.buscarPessoas());
+                    System.out.println("Digite o ID de pessoa que você quer Excluir :");
+                    pessoa.excluir(entradaInt.nextInt());
+                    break;
+                case 5:
+                    System.out.println("Até logo 3.2.1..");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (opcao != 5);
+
+       
     }
 }
